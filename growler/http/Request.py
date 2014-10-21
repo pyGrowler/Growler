51f7414eb8982db056fbd789a7363d605de0188f
Andrew Kubera <andrewkubera@gmail.com>
2014-10-21 14:07:02 -0400

    Added documentation and removed redundancy in HTTPRequest

--- growler/http/Request.py
+++ growler/http/Request.py
@@ -6,6 +6,7 @@ from urllib.parse import (quote, urlparse, parse_qs)
 from . import HTTPParser
 from termcolor import colored
 
+import growler
 
 class HTTPRequest(object):
   """
@@ -15,13 +16,20 @@ class HTTPRequest(object):
   """
   def __init__(self, istream, app = None, delay_processing = False, parser_class = HTTPParser):
     """
-    The HTTPRequest object is all the information you could want about the
-    incoming http connection. It gets passed along with the HTTPResponse object
-    to all the middleware of the app.
-    :istream: an asyncio.StreamReader
-    :app: the Growler app which created this reqest
-    :delay_processing: debug stuff
-    :parser_class: A class which behaves as HTTPParser
+      The HTTPRequest object is all the information you could want about the
+      incoming http connection. It gets passed along with the HTTPResponse object
+      to all the middleware of the app.
+      @param istream: The StreamReader from which to build the request
+      @type istream: asyncio.StreamReader
+
+      @param app: The parent Growler App which created this request
+      @type app: growler.App
+    
+      @param delay_processing: Does nothing
+      @type delay_processing: boolean
+    
+      @param parser_class: The class type which is used to parse the incoming request
+      @type parser_class: HTTPParser
     """
 
     # colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
@@ -41,8 +49,8 @@ class HTTPRequest(object):
   @asyncio.coroutine
   def process(self):
     """
-    Begins processing the incoming stream - first reading in headers. Then if body
-    data is expected, asynchronously read it in.
+      Begins processing the incoming stream - first reading in headers. 
+      If body data is expected, asynchronously read it in.
     """
     # Request Line
     first_line = yield from self._parser.read_next_line()
@@ -58,6 +66,9 @@ class HTTPRequest(object):
       print ( colored("header: {}".format(nheader), self.c))
       nheader = yield from self._parser.read_next_header()
 
+    # Process the headers - specific to the HTTP method (set in process_request_line)
+    self._process_headers()
+
     if not 'content-length' in self.headers:
       self.headers['content-length'] = 0
       self.body.set_result('')
@@ -70,25 +81,32 @@ class HTTPRequest(object):
       # Asynchronously call the parsers' read_body
       asyncio.async(async_read_body(self._parser.read_body))
 
-  @asyncio.coroutine
-  def bodytext(self):
-    """
-    Returns the result from the body. This is an alternate to 'yield from req.body'
-    """
-    body_txt = yield from self.body
-    return body_txt
-
-
   def process_request_line(self, method, request_uri, version):
-    """Checks the values of the three elements of the HTTP header."""
+    """
+      Checks the values of the three elements of the HTTP header.
+    
+      @param method: The HTTP method. GET, POST, etc...
+      @type method: str
+
+      @param request_uri: The uri requested, this gets parsed by 
+        python's own urlparse method
+      @type request_uri: str
+
+      @param version: The HTTP version. Currently only supports the strings 
+        'HTTP/1.1', 'HTTP/1.0' and does NOT distinguish between them - this
+        should be fixed by someone who cares.
+      @type version: str
+    """
     if version not in ('HTTP/1.1', 'HTTP/1.0'):
       raise HTTPErrorVersionNotSupported()
 
+    # save 'method' to self and get the correct function to finish processing
     self.method = method
     self._process_headers = {
-      "GET" : self._process_get_request
+      "GET" : self._process_get_headers
     }.get(method, None)
 
+    # Method not found 
     if self._process_headers == None:
       print ("Unknown HTTP Method '{}'".format(method))
       raise HTTPErrorNotImplemented()
@@ -99,27 +117,20 @@ class HTTPRequest(object):
     self.query = parse_qs(self.parsed_url.query)
 
   def param(self, name, default = None):
-    """Return value of 'name' if found, else return provided 'default'"""
+    """
+      Return value of HTTP parameter 'name' if found, else return provided 
+      'default'
+    
+      @param name: Key to search the query dict for
+      @type name: str
+    
+      @param default: Returned if 'name' is not found in the query dict
+    """
     return self.query[name] if name in self.query.keys() else default
 
-  def _process_get_request(self):
-    """What to do upon """
-    headers = {}
-    while True:
-      header = (yield)
-      if header == None: break
-      key, value = header
-      key = key.lower()
-      # if key is present
-      if key in headers.keys():
-       if isinstance(list, headers[key]):
-         leaders[key].append(value)
-       else:
-         leaders[key] = [leaders[key]]
-      else:
-        headers[key] = value
-    self.headers = headers
-
-    print('[self.headers]', self.headers)
-    yield
-
+  def _process_get_headers(self):
+    """
+      Called upon receiving a GET HTTP request to do specific 'GET' things to the
+      list of headers.
+    """
+    pass
