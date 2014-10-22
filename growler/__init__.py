4b83b39be723ee7f22deaeb2a1437a5503559e3b
Andrew Kubera <andrewkubera@gmail.com>
2014-10-21 23:09:41 -0400

    Finally added the Apache 2 license and a copyright notice in the growler module. Moved init.py to a scripts folder as growler-init.

--- growler/__init__.py
+++ growler/__init__.py
@@ -1,16 +1,31 @@
 #
 # growler/__init__.py
 #
+#   Copyright (c) 2014 Andrew Kubera <andrew.kubera@gmail.com>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 """
 Growler is an http(s) server designed around the asyncio python module which
 imitates Nodejs's express framework, allowing easy creation of complex websites
 using a middleware-based configuration.
 """
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __author__ = "Andrew Kubera"
-__date__ = "Oct 11, 2014"
+__date__ = "Oct 21, 2014"
 __copyright__ = "Copyright 2014, Andrew Kubera"
+__license__ = 'Apache v2.0'
 
 import asyncio
 import ssl
--- setup.py
+++ setup.py
@@ -11,20 +11,39 @@ install_requires = ['termcolor', 'mako', 'pyjade']
 if sys.version_info < (3, 4):
   install_requires.append('asyncio==0.2.1')
 
+description = """
+A web framework covering the asyncio module (PEP 3156), modeled loosely after
+the NodeJS Connect/Express frameworks.
+Growler uses a series of 'middleware' functions to manipulate the request and
+response objects created for each connection. This stream/filter model makes it
+very easy to modularize and extend web applications with any features, backed
+by the power of python.
+
+Currently Growler is single threaded, and not tested very well.
+
+"""
 
 
 setup(
-  name="growler",
-  version="0.0.1",
-  author="Andrew Kubera",
-  author_email="andrew.kubera@gmail.com",
-  description = 'A micro web-framework using asyncio coroutines.',
-  long_description = """A web framework covering the asyncio module (PEP 3156), intending to be similar to the nodejs 'Express' module.""",
+  name= "growler",
+  version= "0.1.0",
+  author= "Andrew Kubera",
+  license= "Apache v2.0",
+  url= "https://github.com/pyGrowler/Growler",
+  author_email= "andrew.kubera@gmail.com",
+  description = "A micro web-framework using asyncio coroutines.",
+  long_description = description,
   classifiers=[
+    "Development Status :: 2 - Pre-Alpha",
+    # "Framework :: Growler",
+    "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.4",
+    "Topic :: Internet :: WWW/HTTP",
+    "Natural Language :: English"
   ],
   install_requires = install_requires,
   packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+  scripts=['scripts/growler-init']
 )
