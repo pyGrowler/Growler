b7b88fc8ffd2d04421ec5d289d2d65e57bd3ce42
Andrew Kubera <andrewkubera@gmail.com>
2015-04-02 13:43:33 +0200

    Reorganized setup.py and made jade an optional install

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@
 #
 
 from os import path
+from glob import glob
 from importlib.machinery import SourceFileLoader
 from setuptools import setup, find_packages
 
 metafile = path.join(".", "growler", "metadata.py")
 metadata = SourceFileLoader("metadata", metafile).load_module()
 
-install_requires = ['termcolor', 'mako', 'pyjade']
+REQUIRES = [
+    'termcolor',
+    'mako'
+]
+
+OPTIONAL_REQUIRES = {
+  'jade': ['pyjade'],
+  ':python_version=="3.3"': ['asyncio>=0.2.1']
+}
 
 long_description = """
 A web framework covering the asyncio module (PEP 3156), modeled loosely after
@@ -32,7 +41,6 @@ setup(
     url="https://github.com/pyGrowler/Growler",
     author_email=metadata.author_email,
     description="A micro web-framework using asyncio coroutines.",
-    long_description=long_description,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
@@ -45,10 +53,11 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
         "Natural Language :: English"
     ],
-    extras_require={':python_version=="3.3"': ['asyncio>=0.2.1']},
+    install_requires=REQUIRES,
+    extras_require=OPTIONAL_REQUIRES,
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
     ),
     platforms='all',
-    scripts=['scripts/growler-init']
+    scripts=glob('scripts/*')
 )
