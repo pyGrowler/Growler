1c021f9f407b2dfe7d53338d8b7203ceeb1b17f3
Andrew Kubera <andrewkubera@gmail.com>
2015-04-06 18:46:00 +0200

    Begun implementing the http responder, and having the growlerprotocol route incoming data to the responder

--- growler/http/responder.py
+++ growler/http/responder.py
@@ -5,5 +5,34 @@
 The Growler class responsible for responding to HTTP requests.
 """
 
+import asyncio
+
 class HTTPResponder():
-    pass
+
+    def __init__(self, protocol):
+        """
+        Construct an HTTPResponder. This should only be called from a growler
+        protocol instance.
+        """
+        print("[HTTPResponder::HTTPResponder]")
+        self.proto = protocol
+        self.data_queue = protocol.data_queue
+        self.proto.loop.create_task(self.data_loop())
+        # self.proto.loop.call_soon(self.data_loop())
+
+    def on_data(self, data):
+        print("HTTP RECEIVED DATA")
+        print(" ", data)
+        self.proto.transport.write(b"Recieved. OK")
+
+    @asyncio.coroutine
+    def data_loop(self):
+        count = 0
+        while True:
+            data = yield from self.data_queue.get()
+            if data is None:
+                break
+            count+=1
+            print("data received",count,"times")
+            # print ("Data Loop returned: ", data)
+        print("Done with loop...")
--- growler/protocol.py
+++ growler/protocol.py
@@ -20,28 +20,33 @@ class GrowlerProtocol(asyncio.Protocol):
 
     responder_type = None
 
-    def __init__(self, responder_type=None):
+    def __init__(self, loop=None, responder_type=None):
         """
         Construct a GrowlerProtocol - this
         """
+        print("[GrowlerProtocol::__init__]", id(self))
         if responder_type is not None:
             self.responder_type = responder_type
+        self.loop = asyncio.get_event_loop if loop is None else loop
+        self.data_queue = asyncio.Queue()
 
     def connection_made(self, transport):
         """
         asyncio.Protocol member - called upon when there is a new socket
-        connection.
+        connection. This creates a new responder (as determined by the member
+        'responder_type') and stores in a list for
+
         @param transport asyncio.Transport: The Transport handling the socket
             communication
         """
-        print("[connection made]")
-        print(" ", transport.__class__)
-        self.responders = [self.responder_type()]
+        print("[GrowlerProtocol::connection_made]")
+
+        self.responders = [self.responder_type(self)]
         self.transport = transport
-        self.hostname = transport.get_extra_info('peername')
+        self.remote_hostname, self.remote_port = transport.get_extra_info('peername')
         self.socket = transport.get_extra_info('socket')
         self.is_done_transmitting = False
-        print("Growler Connection from {}".format(self.hostname))
+        print("Growler Connection from {}:{}".format(self.remote_hostname,self.remote_port))
 
     def connection_lost(self, exc):
         """
@@ -57,16 +62,22 @@ class GrowlerProtocol(asyncio.Protocol):
         asyncio.Protocol member - called upon when there is data to be read
         @param transport bytes: bytes in the latest data transmission
         """
-        print("[server::data_received]", ">>", data)
-        self.responders[-1].push_data(data)
+        self.loop.create_task(self.data_queue.put(data))
+        # self.call_soon(self.self.responders[-1].)
+        # print("[GrowlerProtocol::data_received]", id(self))
+        # print("[server::data_received]", ">>", data)
+        # print("Responders!",self.responders)
+        # asyncio.async(self.responders[-1].data_queue.put, data, loop=self.loop)
+        # self.responders[-1].on_data(data)
 
     def eof_received(self):
         """
         asyncio.Protocol member - called upon when the client signals it will
         not be sending any more data to the server.
         """
+        self.loop.create_task(self.data_queue.put(None))
         self.is_done_transmitting = True
-        print("[eof_received]")
+        print("[GrowlerProtocol::eof_received]")
 
 
 # Or should this be called HTTPGrowlerProtocol?
