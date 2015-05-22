c0a3dea7fb52c0cf7bfd8e84def14a673d1e6d2c
Andrew Kubera <andrewkubera@gmail.com>
2015-05-22 14:37:35 -0400

    Version bump to 0.5.0 - now considering project in 'Beta' mode

--- growler/metadata.py
+++ growler/metadata.py
@@ -4,14 +4,14 @@
 """Project Metadata"""
 
 package = 'growler'
-project = 'A New Web Framework'
+project = 'Micro-Framework using asyncio corutines'
 project_no_spaces = project.replace(' ', '')
 
-version_info = (0, 2, 0)
+version_info = (0, 5, 0)
 version = '.'.join(map(str, version_info))
 version_name = ''.join((project, 'version', version))
 
-date = "May 16, 2015"
+date = "May 22, 2015"
 author = "Andrew Kubera"
 author_email = "andrew.kubera@gmail.com"
 copyright = "Copyright 2015, Andrew Kubera"
