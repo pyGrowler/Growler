8dc56081d52ed750063d8868a6aa94097c4cc283
Andrew Kubera <andrewkubera@gmail.com>
2015-07-03 23:56:26 -0400

    Updated year in the copyright info in license header in growler/__init__.py and added ignore comment so flake8 will ignore the file

--- growler/__init__.py
+++ growler/__init__.py
@@ -1,7 +1,9 @@
 #
 # growler/__init__.py
 #
-#   Copyright (c) 2014 Andrew Kubera <andrew.kubera@gmail.com>
+# flake8: noqa
+#
+#   Copyright (c) 2015 Andrew Kubera <andrew.kubera@gmail.com>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
