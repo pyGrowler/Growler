fea936409c6cd271277c3a5d75d007249f047574
Andrew Kubera <andrewkubera@gmail.com>
2015-07-24 01:25:00 -0400

    Version bump to v0.5.5

--- growler/metadata.py
+++ growler/metadata.py
@@ -7,11 +7,11 @@ package = 'growler'
 project = 'Micro-Framework using asyncio corutines'
 project_no_spaces = project.replace(' ', '')
 
-version_info = (0, 5, 0)
+version_info = (0, 5, 5)
 version = '.'.join(map(str, version_info))
 version_name = ''.join((project, 'version', version))
 
-date = "May 22, 2015"
+date = "July 24, 2015"
 author = "Andrew Kubera"
 author_email = "andrew.kubera@gmail.com"
 copyright = "Copyright 2015, Andrew Kubera"
