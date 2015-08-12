8404e696676faf97b0b20c916ce44b149730a065
Andrew Kubera <andrewkubera@gmail.com>
2015-08-12 12:12:42 -0400

    growler.http.methods: Formatting change and removed extraneous debug code that should not have been there.

--- growler/http/methods.py
+++ growler/http/methods.py
@@ -1,17 +1,18 @@
 #
 # growler/http/methods.py
 #
-
+# flake8: noqa
+#
 
 import enum
 
 
 class HTTPMethod(enum.IntEnum):
-    ALL =    0b011111
-    GET =    0b000001
-    POST =   0b000010
+    ALL    = 0b011111
+    GET    = 0b000001
+    POST   = 0b000010
     DELETE = 0b000100
-    PUT =    0b001000
+    PUT    = 0b001000
 
 
 string_to_method = {
@@ -20,7 +21,3 @@ string_to_method = {
     "DELETE": HTTPMethod.DELETE,
     "PUT": HTTPMethod.PUT,
 }
-
-x = HTTPMethod.GET | HTTPMethod.POST
-print(x)
-assert None
