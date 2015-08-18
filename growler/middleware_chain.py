3315d72b03e49b1005bd0b2511aa8ed27de2400f
Andrew Kubera <andrewkubera@gmail.com>
2015-08-18 17:32:44 -0400

    growler.middleware_chain: "Add" method adds middleware to the chain. __contains__ finds a function anywhere in the chain

--- growler/middleware_chain.py
+++ growler/middleware_chain.py
@@ -10,6 +10,9 @@ class MiddlewareChain:
     Class handling the storage and retreival of growler middleware functions.
     """
 
+    def __init__(self):
+        self.mw_list = []
+
     def __call__(self, path):
         """
         Generator yielding the middleware which is matches the path provided.
@@ -18,7 +21,14 @@ class MiddlewareChain:
         :type path: str
         """
 
-    def add(self, method, path, func):
+    def add(self, method_mask, path, func):
         """
         Add a function to the middleware chain, listening on func
         """
+        self.mw_list.append((func,))
+
+    def __contains__(self, func):
+        for mw in self.mw_list:
+            if func == mw[0]:
+                return True
+        return False
--- tests/test_middleware_chain.py
+++ tests/test_middleware_chain.py
@@ -4,7 +4,7 @@
 
 import growler
 import pytest
-
+from unittest import mock
 
 @pytest.fixture
 def chain():
@@ -13,3 +13,9 @@ def chain():
 
 def test_constructor(chain):
     assert isinstance(chain, growler.MiddlewareChain)
+
+
+def test_add(chain):
+    func = mock.Mock()
+    chain.add(0x1, '/', func)
+    assert func in chain
