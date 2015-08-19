2c61ff51eab31452f27fb13e13195d70b337b20f
Andrew Kubera <andrewkubera@gmail.com>
2015-08-19 05:13:55 -0400

    middleware_chain: Implemented a recursive algorithm for generating the middleware and error handlers

--- growler/middleware_chain.py
+++ growler/middleware_chain.py
@@ -15,8 +15,9 @@ class MiddlewareChain:
 
     MiddlewareTuple = namedtuple('MiddlewareTuple', ['func',
                                                      'path',
-                                                     'err',
                                                      'mask',
+                                                     'is_errorhandler',
+                                                     'is_subchain',
                                                      ])
 
     def __init__(self):
@@ -29,19 +30,48 @@ class MiddlewareChain:
         :param path: url path of the request.
         :type path: str
         """
+        error_handler_stack = []
+        err = None
         for mw in self.mw_list:
-            if (method & mw.mask) and (path.startswith(mw.path)):
-                yield mw.func
+            method_matches = method & mw.mask
+            path_matches = path.startswith(mw.path)
+
+            if not (method_matches and path_matches):
+                continue
+
+            if mw.is_subchain:
+                subchain = mw.func(method, path[len(mw.path):])
+                for sub_mw in subchain:
+                    err = yield sub_mw.func
+                    if err:
+                        sub_mw.send(err)
+
+            elif mw.is_errorhandler:
+                error_handler_stack.append(mw.func)
+
+            else:
+                err = yield mw.func
+
+            if err:
+                break
+
+        if err:
+            for errhandler in reversed(error_handler_stack):
+                new_error = yield errhandler
+                if new_error:
+                    pass
 
     def add(self, method_mask, path, func):
         """
         Add a function to the middleware chain, listening on func
         """
         is_err = len(signature(func).parameters) == 3
+        is_subchain = isinstance(func, MiddlewareChain)
         tup = self.MiddlewareTuple(func=func,
                                    mask=method_mask,
                                    path=path,
-                                   err=is_err,)
+                                   is_errorhandler=is_err,
+                                   is_subchain=is_subchain,)
         self.mw_list.append(tup)
 
     def __contains__(self, func):
