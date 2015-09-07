ef80b293edb0bff0c3f50e2705f4c94c23d7d888
Andrew Kubera <andrewkubera@gmail.com>
2015-09-06 23:59:35 -0400

    growler.ext: Added a docstring to GrowlerExtensionImporter getattr method

--- growler/ext/__init__.py
+++ growler/ext/__init__.py
@@ -10,7 +10,11 @@ import sys
 
 class GrowlerExtensionImporter:
 
-    def __getattr__(cls):
+    def __getattr__(cls, module_name):
+        """
+        Get the 'attribute' of growler.ext, which looks for the module in the
+        python virtual namespace growler_ext
+        """
         pass
 
 sys.modules[__name__] = GrowlerExtensionImporter(__name__)
