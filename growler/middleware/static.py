d4b1152095cbfbed425d5e393857b58bae3bc4ce
Andrew Kubera <andrewkubera@gmail.com>
2015-09-24 02:00:18 -0400

    middleware.static: __call__ method is no longer an unnecessary coroutine. Improved logging and documentation.

--- growler/middleware/static.py
+++ growler/middleware/static.py
@@ -6,30 +6,45 @@
 import asyncio
 import os
 import mimetypes
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class Static():
     """
     Static middleware catches any uri paths which match a filesystem file and
     serves that file.
+
+    :param path: The directory path to search for files. If this is a list, the
+                 paths will be path-joined automatically.
+    :type path: str/list
     """
 
     def __init__(self, path):
+        # if list, do a pathjoin
         if isinstance(path, list):
             path = os.path.join(*path)
-        self.path = os.path.abspath(path)  # os.getcwd() + path
-        print("[Static] Serving static files out of {}".format(self.path))
-        if not os.path.exists(self.path):
-            print("[Static] Error:", "No path exists at {}".format(self.path))
+
+        # store as the computed absolute path, to avoid unexpected relative
+        # path redirection
+        self.path = os.path.abspath(path)
+
+        # ensure that path exists
+        if not os.path.isdir(self.path):
+            log.error("[Static] No path exists at {}".format(self.path))
             raise Exception("Path '{}' does not exist.".format(self.path))
 
-    @asyncio.coroutine
+        log.info("%d Serving static files out of %s" % (id(self), self.path))
+
     def __call__(self, req, res):
-        print("[Static] ", req.path)
+        """
+        Middleware handle function. Simply checks if matching path is a file,
+        attempts to guess the file type, and sends the file.
+        """
         filename = self.path + req.path
         if os.path.isfile(filename):
             mime = mimetypes.guess_type(filename)
-            print("  Found: {} ({})".format(filename, mime[0]))
             res.set_type(mime[0])
-            res.send_file(self.path + req.path)
-            print("  DONE!")
+            res.send_file(filename)
+            log.info("%d Sent %s (%s)" % (id(self), filename, mime[0]))
