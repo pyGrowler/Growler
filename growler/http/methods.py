97ebd3ac4c54c6740249ecc64bdf636e6e1a5f94
Andrew Kubera <andrewkubera@gmail.com>
2015-11-27 12:25:28 -0500

    Added HEAD http method and a "method to string" function to the http.methods package

--- growler/http/methods.py
+++ growler/http/methods.py
@@ -13,6 +13,7 @@ class HTTPMethod(enum.IntEnum):
     POST   = 0b000010
     DELETE = 0b000100
     PUT    = 0b001000
+    HEAD   = 0b010000
 
 
 string_to_method = {
@@ -20,4 +21,8 @@ string_to_method = {
     "POST": HTTPMethod.POST,
     "DELETE": HTTPMethod.DELETE,
     "PUT": HTTPMethod.PUT,
+    "HEAD": HTTPMethod.HEAD,
 }
+
+
+method_to_string = dict((v,k) for k, v in string_to_method.items())
