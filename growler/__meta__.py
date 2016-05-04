a057adbed0dbde6e6a15feee59add0f2889b546a
Andrew Kubera <andrewkubera@gmail.com>
2016-05-04 13:16:15 -0400

    Version 0.7.2

--- growler/__meta__.py
+++ growler/__meta__.py
@@ -6,11 +6,11 @@
 package = 'growler'
 project = 'Web framework using standard python asyncio'
 
-version_info = (0, 7, 1)
+version_info = (0, 7, 2)
 version = '.'.join(map(str, version_info))
 version_name = ''.join((project, 'version', version))
 
-date = "Apr 18, 2016"
+date = "May 04, 2016"
 author = "Andrew Kubera"
 author_email = "andrew.kubera@gmail.com"
 copyright = "Copyright 2016, Andrew Kubera"
