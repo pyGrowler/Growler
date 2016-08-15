419e9947d91b5ffeeac3886d986cac81ff609d17
Andrew Kubera <andrewkubera@gmail.com>
2016-08-15 17:18:02 +0200

    middleware.responsetime: Added tests for coverage and documentation of constructor

--- growler/middleware/responsetime.py
+++ growler/middleware/responsetime.py
@@ -28,15 +28,19 @@ class ResponseTime:
                  header="X-Response-Time",
                  suffix=True,
                  clobber_header=False):
+        """
+        Construct ResponseTime middleware.
 
-        factor = self.UNIT_TO_FACTOR_MAP[units]
-
-        def format_timediff(td):
-            return str(round(factor * td, digits))
-
-        self.format = format_timediff
+        Parameters:
+            digits (int): precision
+            log (bool): Writes the time difference to the log
+            units (str): Time units (default: milliseconds 'ms')
+            header (str): Name of header to send response time as
+            suffix (bool): Whether to format with
+        """
         self.units = units
-        self.header = header
+        self.header_name = header
+        self.digits = digits
         self.suffix = suffix
         self.clobber_header = clobber_header
 
@@ -45,11 +49,15 @@ class ResponseTime:
 
         def on_header_send():
             # if header already exists, do NOT clobber it
-            if not self.clobber_header and self.header in res.headers:
+            if not self.clobber_header and self.header_name in res.headers:
                 return
 
-            dt = self.format(time.monotonic() - start_time)
+            dt = self.format_timediff(time.monotonic() - start_time)
             val = "{}{}".format(dt, self.units) if self.suffix else dt
-            res.set(self.header, val)
+            res.set(self.header_name, val)
 
         res.on_headers(on_header_send)
+
+    def format_timediff(self, td):
+        factor = self.UNIT_TO_FACTOR_MAP[self.units]
+        return str(round(factor * td, self.digits))
