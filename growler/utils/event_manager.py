69084e5eefcbde9f1b1da69561292bc473994f1a
Andrew Kubera <andrewkubera@gmail.com>
2016-08-15 16:33:15 +0200

    Events: Finally commiting an events manager class for andling async callbacks

--- growler/utils/event_manager.py
+++ growler/utils/event_manager.py
@@ -3,6 +3,12 @@
 #
 
 import asyncio
+try:
+    from types import CoroutineType, GeneratorType
+except ImportError: # shim until python3.4 support is dropped
+    from types import FunctionType as CoroutineType, GeneratorType
+
+from collections import defaultdict
 
 
 def event_emitter(cls_=None, *, events=('*',), loop=None):
@@ -45,6 +51,7 @@ def event_emitter(cls_=None, *, events=('*',), loop=None):
 
             return self
 
+        # async def emit(self, name):
         @asyncio.coroutine
         def emit(self, name):
             """
@@ -52,6 +59,8 @@ def event_emitter(cls_=None, *, events=('*',), loop=None):
             identified by 'name'
             """
             for cb in event_dict[name]:
+                # if isinstance(cb, CoroutineType):
+                    # await cb()
                 if asyncio.iscoroutinefunction(cb):
                     yield from cb()
                 else:
@@ -70,3 +79,72 @@ def event_emitter(cls_=None, *, events=('*',), loop=None):
 
 def emits(pre=None, *, post=None):
     pass
+
+
+class Events:
+    """
+    A high-level container of asynchronous callback events.
+    Objects of this type are intended to be used as a member of a class.
+
+    The standard usage is to have a member named 'events' to which you
+    add various callbacks using on.
+
+    Upon this supports both synchronous and asynchronous callbacks.
+
+    In the future, I'd like to have :before & :after tags to strictly
+    define whether when callbacks are called relative to another
+    function. The current way to do this is to have two events, ``before_foo``
+    and ``after_foo`` and it's up to the implementation of 'foo' to ensure
+    that these are called appropriately.
+
+
+    Example:
+
+    >>> my_obj.events.on('foo', bar)
+    >>> # some code
+    >>> async def run_stuff():
+    >>>     await my_obj.events.emit('foo')  # bar() is called
+
+    """
+
+    def __init__(self, *event_names):
+        """
+        Construct Events object with a set of allowed event names.
+        If no event names are given, then all events are allowed.
+
+
+        """
+        if ... in event_names or event_names == ():
+            self._event_list = defaultdict(list)
+        else:
+            self._event_list = {name: [] for name in event_names}
+
+
+    def on(self, name, _callback=None):
+        """
+        Add a callback to the event named 'name'.
+        Returns callback object for decorationable calls.
+        """
+        # this must be a decorator
+        if _callback is None:
+            return lambda cb: self.on(name, cb)
+
+        if not callable(_callback) and not isinstance(_callback, GeneratorType):
+            msg = "Callback not callable: {0!r}".format(_callback)
+            raise ValueError(msg)
+
+        self._event_list[name].append(_callback)
+        return _callback
+
+    # async def emit(self, name):
+    @asyncio.coroutine
+    def emit(self, name):
+        """
+        Add a callback to the event named 'name'.
+        Returns this object for chained 'on' calls.
+        """
+        for cb in self._event_list[name]:
+            if isinstance(cb, (CoroutineType, GeneratorType)):
+                yield from cb
+            else:
+                cb()
--- tests/test_event_emitter.py
+++ tests/test_event_emitter.py
@@ -2,20 +2,21 @@
 # tests/test_event_emitter
 #
 
-from growler.utils.event_manager import event_emitter
-from unittest import mock
 import pytest
 import asyncio
 
-
-@pytest.fixture
-def loop():
-    return asyncio.get_event_loop()
+from unittest import mock
+from growler.utils.event_manager import event_emitter, Events
 
 
 @pytest.fixture
-def mock_func():
-    return mock.MagicMock(asyncio.get_event_loop)
+def loop(event_loop):
+    return event_loop
+
+
+@pytest.fixture
+def mock_func(loop):
+    return mock.create_autospec(lambda: None)
 
 
 @event_emitter
@@ -59,3 +60,62 @@ def test_on_good_name(loop, mock_func):
     emit_coro = e.emit('good')
     loop.run_until_complete(emit_coro)
     assert mock_func.called
+
+
+# @pytest.mark.asyncio
+# async def test_coro_callback(): #loop, mock_func):
+#     e = EEE()
+#     async def foo():
+#         return 10
+#     e.on('good', foo)
+#     emit_coro = e.emit('good')
+#     await emit_coro
+#     assert mock_func.called
+
+
+def test_events_constructor_empty():
+    e = Events()
+    e.on('anything', lambda: print())
+
+
+def test_events_constructor_nonempty():
+    e = Events('foo')
+    with pytest.raises(KeyError):
+        e.on('anything', lambda: print())
+
+def test_events_on_typecheck():
+    e = Events('foo')
+    with pytest.raises(ValueError):
+        e.on('anything', 10)
+
+@pytest.mark.asyncio
+def test_events_on_decorator():
+    e = Events('foo')
+    m = mock.MagicMock()
+
+    @e.on("foo")
+    def doit():
+        m()
+    yield from e.emit("foo")
+    assert m.called
+
+@pytest.mark.asyncio
+def test_events_on():
+    e = Events('foo')
+    m = mock.MagicMock()
+    e.on('foo', m)
+    yield from e.emit('foo')
+    assert m.called
+
+@pytest.mark.asyncio
+def test_events_on_async():
+    e = Events('foo')
+    m = mock.MagicMock()
+
+    @asyncio.coroutine
+    def foo():
+        m()
+
+    e.on('foo', foo())
+    yield from e.emit('foo')
+    assert m.called
