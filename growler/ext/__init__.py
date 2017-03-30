19d303bfe65a9288f5c439ed848c28e17d7b87af
Andrew Kubera <andrew.michael.kubera@floss.email>
2017-03-30 16:43:15 -0400

    growler.ext: Use pkg_resources for extension manager

--- growler/ext/__init__.py
+++ growler/ext/__init__.py
@@ -6,35 +6,12 @@ Virtual namespace for other pacakges to extend the growler server
 """
 
 import sys
-from importlib import (
-    import_module,
-)
+import pkg_resources
 
+# load all items in 'growler.ext' and add to local namespace
+for obj in pkg_resources.iter_entry_points(group='growler.ext'):
+   locals()[obj.name] = obj.load()
 
-class GrowlerExtensionImporter:
-
-    __path__ = 'growler.ext'
-    __name__ = 'GrowlerExtensionImporter'
-    __mods__ = {}
-
-    def __getattr__(self, module_name):
-        """
-        Get the 'attribute' of growler.ext, which looks for the module in the
-        python virtual namespace growler_ext
-        """
-        try:
-            result = self.__mods__[module_name]
-        except KeyError:
-            # import the 'real' module
-            result = import_module('growler_ext.' + module_name)
-
-            # store alias in sys.modules
-            alias_mod_name = 'growler.ext.' + module_name
-            sys.modules[alias_mod_name] = result
-
-            # cache in this object
-            self.__mods__[module_name] = result
-
-        return result
-
-sys.modules[__name__] = GrowlerExtensionImporter()
+# clean local namespace
+del pkg_resources
+del sys
